a71b90c60ac452deb8a9b0f9257d2eb998090a1a
oh <vladultimate@gmail.com>
2021-09-07 16:16:08 +0300

    Work

--- style/indx.js
+++ style/indx.js
@@ -4,37 +4,6 @@
     3:[id-3,Jack, Cros],
 }
 
-const user1 = {
-    id: 21,
-    name: Tom,
-    surname: Fox,
-}
-const user2 = {
-    id: 322,
-    name: Ann,
-    surname: Smith,
-}
-const user3 = {
-    id: 417,
-    name: Jack,
-    surname: Cros,
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let map = new Map();
-map.set(1,{name:"Tom",surname:"Fox",})
-console.log(map.get(1))
 */
 const worker = {
     name: 'Jack',
@@ -46,13 +15,12 @@ const worker = {
 
     },
     sall: function (){
-        
         if(this.expiriance>=5 && this.expiriance <= 7){
            console.log(("Bonus:")+((this.salary/100)*5))
         }else if(this.expiriance<=8){
             console.log(("Bonus:")+((this.salary/100)*8))
-        }
+        }else{console.log("You doesn`t have some bonus")}
     },
 }
-//worker.greet();
+worker.greet();
 worker.sall();
